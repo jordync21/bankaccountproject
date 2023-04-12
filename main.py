c316e312e477f2cc2f47aaa05679d78410c0a4f8
jordyn <justjordyncobb@gmail.com>
2023-04-12 16:48:30 -0400

    cuts off program when 4 is input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bank_account import BankAccount
+import os
 
-
+clear = os.system('cls')
 
 is_on = True
 
@@ -10,9 +11,11 @@ while is_on:
     pw = input("Please enter password: ")
     info = BankAccount(username=user, password=pw)
     while info.cred_valid():
-        command = int(input("1 = Check Balance \n2 = Withdraw \n3 = Deposit: "))
-        info.view_balance(command)
-        info.withdraw_balance(command)
-        info.deposit_balance(command)
-        if command == 'exit':
+        command = int(input("1 = Check Balance \n2 = Withdraw \n3 = Deposit \n4 = Exit: "))
+        if command == 4:
             is_on = False
+            break
+        else:
+            info.view_balance(command)
+            info.withdraw_balance(command)
+            info.deposit_balance(command)
